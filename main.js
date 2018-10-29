2f91511cece1dbef923a4eee8f7e6c8cb80ae5f2
carissa morrow <carissamorrow@hotmail.com>
2018-10-29 11:47:51 -0600

    carissa

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-let myFavNum = 22//string
-let myFavColor = "green"//number
-let isMale = true//boolean
-
-let jake = {
-  name: "Jake",
-  age: 32,
+let carissa = {
+  name: "Carissa",
+  age: 30,
   favoriteColor: "green",
-  male: true
-}
+  male: false,
+  eyes: {
+    left: {
+      color: "green",
+      vision: "20/20"
+    },
+    right: {
+      color: "green",
+      vision: "20/20"
+    },
+    pets: {
+      dog1: {
+        name: "Tucker"
+      },
+      dog2: {
+        name: "Brody"
+      },
+    }
+  }
+}//object
+
+
+//array(aka lists or collections)
+let humans = [carissa]
+
+let story1 = `my name is ${carissa.name} and my favorite color is ${carissa.favoriteColor}`
+
+console.log(story1)
