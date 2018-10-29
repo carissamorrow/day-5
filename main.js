a49abb16bbe921d98fcfc8fab2022e3081ffb344
carissa morrow <carissamorrow@hotmail.com>
2018-10-29 13:19:41 -0600

    addedfriends

--- main.js
+++ main.js
@@ -3,51 +3,55 @@ let carissa = {
   age: 30,
   favoriteColor: "green",
   male: false,
-  eyes: {
-    left: {
-      color: "green",
-      vision: "20/20"
-    },
-    right: {
-      color: "green",
-      vision: "20/20"
-    },
-    pets: {
-      dog1: {
-        name: "Tucker"
-      },
-      dog2: {
-        name: "Brody"
-      },
-    }
-  }
+  // eyes: {
+  //   left: {
+  //     color: "green",
+  //     vision: "20/20"
+  //   },
+  //   right: {
+  //     color: "green",
+  //     vision: "20/20"
+  //   },
+  //   pets: {
+  //     dog1: {
+  //       name: "Tucker"
+  //     },
+  //     dog2: {
+  //       name: "Brody"
+  //     },
+  //   }
+  // },
+  friends: []
 }
 let chris = {
   name: "Chris",
   age: 32,
   favoriteColor: "blue",
   male: true,
-  friends: ["carissa"]
+  friends: [carissa]
 }
 let natalie = {
   name: "Natalie",
   age: 62,
   favoriteColor: "pink",
   male: false,
-  friends: ["carissa, chris"]
+  friends: [carissa, chris]
 }
 let joseph = {
   name: "Joe",
   age: 62,
   favoriteColor: "blue",
   male: true,
-  friends: ["carissa, chris, natalie"]
+  friends: [carissa, chris, natalie]
 }
+let humans = [carissa, chris, natalie, joseph]
 
+natalie.friends.push(joseph)
+chris.friends.push(joseph)
 
-//array(aka lists or collections)
-let humans = [carissa]
 
 let story1 = `my name is ${carissa.name} and my favorite color is ${carissa.favoriteColor}`
-let story2 = `my name is ${chris.name} and my favorite color is ${chris.favoriteColor}`
+console.log(story1)
+
+let story2 = `my name is ${chris.name} an my favorite age is ${chris.age}`
 console.log(story2)
